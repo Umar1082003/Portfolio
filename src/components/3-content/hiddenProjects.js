8f866a6d6f9792a3dd17538437d7021b057dcd70
umar <omarabdelwahabsaid@gmail.com>
2025-09-23 17:02:49 +0300

    add projects & fix issues

---
+++ src/components/3-content/hiddenProjects.js
@@ -0,0 +1,22 @@
+export const proj = [
+  {
+    id: 6,
+    title: "Bootstrap Landing Page",
+    img: "/Portfolio/assets/landingPageBootstrap.png",
+    category: "bootstrap",
+    demo: "https://umar1082003.github.io/Landing-Page-Bootstrap/",
+    link: "https://github.com/Umar1082003/Landing-Page-Bootstrap",
+    description:
+      "landing page built with Bootstrap. It features a responsive design with a clean layout and modern styling. The app uses Bootstrap components for the layout and styling.",
+  },
+  {
+    id: 1,
+    title: "Modern Landing Page",
+    img: "/Portfolio/assets/landingPageReact.png",
+    category: "CSS",
+    link: "https://github.com/Umar1082003/Landing-Page-With-React",
+    demo: "https://umar1082003.github.io/Landing-Page-With-React/",
+    description:
+      "A simple landing page built using HTML and CSS. It features a responsive design with a clean layout and modern styling and some animations.",
+  },
+];
